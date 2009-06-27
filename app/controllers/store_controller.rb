7a0af56359d318436bd11d6ecebf61dd92da41b8
sumit <sumitguha1@gmail.com>
2009-06-26 17:25:32 -0700

    added AJAX to shopping cart and highlighted new entry with effect.js effect

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,14 +1,16 @@
 class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
-    @cart = find_cart
+    @cart = find_cart  
   end
 
   def add_to_cart
     product = Product.find(params[:id]) 
-    @cart = find_cart                   
-    @cart.add_product(product)
-    redirect_to_index
+    @cart = find_cart
+    @current_item = @cart.add_product(product)
+      respond_to do |format|
+        format.js
+      end  
   rescue ActiveRecord::RecordNotFound
     logger.error("Attempt to access invalid product #{params[:id]}")
     redirect_to_index("Invalid product")
